c596cc86e0a3760a04e4300674a4d1a8f1abee67
jhackett1 <joshua.hackett93@gmail.com>
2019-03-08 13:30:12 +0000

    use postgres in dev

--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,14 @@
 
 ActiveRecord::Schema.define(version: 2019_03_07_121051) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "hours", force: :cascade do |t|
     t.string "description"
     t.datetime "date"
     t.integer "hours"
-    t.integer "project_id"
+    t.bigint "project_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["project_id"], name: "index_hours_on_project_id"
@@ -25,9 +28,10 @@ ActiveRecord::Schema.define(version: 2019_03_07_121051) do
   create_table "projects", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", null: false
-    t.text "address", null: false
+    t.text "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "hours", "projects"
 end
